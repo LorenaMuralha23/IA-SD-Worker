bd741a4170bfaad993fc2b178746a09964e4dda6
Lorena Muralha <mlmuralha2301@gmail.com>
2025-02-08 11:25:48 -0300

    feat: Comunicação e treinamento de modelo implementado

---
+++ worker.py
@@ -0,0 +1,83 @@
+import json
+import socket
+import os
+from queue import Queue
+from multiprocessing import Process, active_children
+from cnn import CNN
+import torch
+from torchvision import datasets
+from torchvision.transforms import v2
+import time
+from main import Main
+
+
+task_queue = Queue()
+main = Main()
+
+data_transforms = main.define_transforms(224, 224)
+train_data, validation_data, test_data = main.read_images(data_transforms)
+cnn = CNN(train_data, validation_data, test_data, 8)
+
+
+def receiveTask(receivedJson):
+    if 'data' in receivedJson and isinstance(receivedJson.get('data'), list):
+        for combination in receivedJson.get('data'):
+            processTask(combination)
+    else:
+        print("O JSON recebido não contém um campo 'data' válido.")
+
+
+def sendJson(jsonToSend):
+    return jsonToSend
+
+
+def createJson(status):
+    try:
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            ip_address = s.getsockname()[0]
+
+        num_cores = os.cpu_count()
+
+        createdJson = {
+            "machine_id": 'worker-01',
+            "ip_address": ip_address,
+            "port": 5000,
+            "status": status,
+            "num_cores": num_cores
+        }
+
+        createdJson = json.dumps(createdJson, indent=4)
+
+        return createdJson
+
+    except Exception as e:
+        return json.dumps({"error": str(e)}, indent=4)
+
+
+def process_task_wrapper(cnn, repl, mn, epochs, lr, wd):
+    from workerServer import sendToGroup
+    main = Main()
+
+    taskResult = main.processTask(cnn, repl, mn, epochs, lr, wd)
+
+    sendToGroup(taskResult)
+
+
+def processTask(combination):
+
+    repl = combination.get('replications')
+    mn = combination.get('model_name')
+    epochs = combination.get('epochs')
+    lr = combination.get('learning_rate')
+    wd = combination.get('weight_decay')
+    task = Process(target=process_task_wrapper,
+                   args=(cnn, repl, mn, epochs, lr, wd))
+    task.start()
+    print(f"Processo iniciado: PID={task.pid}, Nome={task.name}")
+    print(f"Processos ativos no momento: {len(active_children())}")
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ workerServer.py
@@ -0,0 +1,60 @@
+import random
+import socket
+import struct
+import json
+
+from worker import receiveTask, createJson
+
+
+MULTICAST_GROUP = '224.1.1.1'
+PORT = 5007
+MACHINE_ID = 0
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                1)
+
+sock.bind(('', PORT))
+
+mreq = struct.pack("4s4s", socket.inet_aton(
+    MULTICAST_GROUP), socket.inet_aton("0.0.0.0"))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+print("Cliente Multicast aguardando mensagens...")
+
+
+def receiveMessage():
+    cont = 0
+    while (True):
+        data, addr = sock.recvfrom(4096)
+        try:
+            decoded_data = data.decode()
+            receivedJson = json.loads(decoded_data.strip())
+
+            if (type(receivedJson) is dict):
+                if receivedJson.get("machine_id") != MACHINE_ID:  # <-
+                    receiveTask(receivedJson)
+
+            else:
+                print("O JSON recebido não é um dicionário válido.")
+
+            cont += 1
+        except json.JSONDecodeError:
+            print(f"Erro ao decodificar JSON de {addr}: {data.decode()}")
+
+
+def sendToGroup(taskToSend):
+    print("Json a ser enviado: " + json.dumps(taskToSend))
+    sock.sendto(taskToSend.encode(), (MULTICAST_GROUP, PORT))
+    receiveMessage()
+
+
+def generate_unique_id():
+    return random.randint(1, 10000)
+
+
+if __name__ == "__main__":
+    MACHINE_ID = generate_unique_id()
+    print("ID GERADO: " , MACHINE_ID)
+    createdJson = createJson("ONLINE")
+    sendToGroup(createdJson)
